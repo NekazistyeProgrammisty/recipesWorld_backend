25acd8ab50ea6990e9cf93d3e41770b9af7a395c
chechkysh <dentag240502@mail.ru>
2022-04-19 04:38:28 +0300

    Find recipe by products is done, but not well tested

--- src/main/java/com/restmvc/foodboard/service/RecipeService.java
+++ src/main/java/com/restmvc/foodboard/service/RecipeService.java
@@ -12,10 +12,7 @@ import com.restmvc.foodboard.repository.RecipeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class RecipeService {
@@ -28,6 +25,7 @@ public class RecipeService {
 
     public void newRecipe(RecipeEntity recipe) throws AlreadyExistException {
         if(recRepo.findBytitle(recipe.getTitle())==null){
+                recipe.setProductsNum(0);
                 recRepo.save(recipe);
         }else throw new AlreadyExistException("Блюдо с таким названием уже существует");
 
@@ -47,7 +45,9 @@ public class RecipeService {
             pr.setProduct(product);
             pr.setProductImportance(importance);
             pr.setRecipe(recipe);
+            recipe.setProductsNum(recipe.getProductsNum()+1);
             prodRecRepo.save(pr);
+            recRepo.save(recipe);
         }else throw new NotFoundedException("Блюда "+recId+" нет в базе");
       }
 
@@ -100,37 +100,92 @@ public class RecipeService {
         }else throw new NotFoundedException("Рецепты не найдены");
     }
 
-
+/*Алгоритм выдачи рецептов по продуктам пользователя:
+* Если в рецепте есть хоть один продукт, которого нет у пользователя, и у этого продукта важность >5, то этот рецепт не подходит.
+* Соответственно если рцепт состоит только из продуктов, которые есть у пользователя, или если в рецепте продукты, которых нет у пользователя, не важны(<5), то такой рецепт подходит
+* Если поставить всем продуктам важность>5, то алгоритм будет выдавать только те рецепты, которые целиком состоят из продуктов пользователя*/
     public ArrayList<RecipeModelPure> findRecipesByProducts(ArrayList<UserProductsEntity> productsList){
-        //РАБОТАЕТ НЕПРАВИЛЬНО, ОБЯЗАТЕЛЬНО НУЖНО СДЕЛАТЬ ТАК, ЧТОБЫ ПЕРВЫЙ ПРОДУКТ БЫЛ С НАИМ. КОЛИЧЕСТВОМ РЕЦЕПТОВ!!!!!!!!!!!!!!
         Integer productsCount = productsList.size();
-        Map<RecipeEntity,Integer> recipeMap = new HashMap<>();
+        ArrayList<RecipeEntity> recipeEntities = new ArrayList<>();
+        ArrayList<RecipeEntity> recipeEntitiesForDelition = new ArrayList<>();
         ArrayList<RecipeModelPure> finalRecipes = new ArrayList<>();
-        UserProductsEntity usersProdFirst = productsList.get(0);
-        for(ProdRecEntity prodRec:usersProdFirst.getProduct().getRecipes()){
-            recipeMap.put(prodRec.getRecipe(),1);
-        }
-        productsList.remove(usersProdFirst);
-        for(UserProductsEntity usersProd:productsList){
-            for(ProdRecEntity prodRec:usersProd.getProduct().getRecipes()){
-                RecipeEntity recipe =prodRec.getRecipe();
-                if(recipeMap.containsKey(recipe)){
-                    recipeMap.replace(recipe,recipeMap.get(recipe)+1);
+        ArrayList<ProdRecEntity> prodRecListOfProducts = new ArrayList<>();
+        for(UserProductsEntity userProds:productsList){//составляем список ВСЕХ рецептов в которых используются переданные продукты.
+            ArrayList<ProdRecEntity> prodRecList = new ArrayList<>(userProds.getProduct().getRecipes());
+            prodRecListOfProducts.addAll(prodRecList);//вместе с тем составляем список prodRec сущностей переданных ПРОДУКТОВ
+            for(ProdRecEntity prodRecEntity:prodRecList){
+                if(!recipeEntities.contains(prodRecEntity.getRecipe())) {
+                    recipeEntities.add(prodRecEntity.getRecipe());
+                    System.out.println("added " + prodRecEntity.getRecipe().getTitle());
                 }
             }
         }
-        for(RecipeEntity key:recipeMap.keySet()){
-            if(recipeMap.get(key).equals(productsCount)){
-                RecipeModelPure model = new RecipeModelPure();
-                model.toModel(key);
+        for(RecipeEntity recipe:recipeEntities){
+            ArrayList<ProdRecEntity> prodRecListOfRecipe = new ArrayList<>(recipe.getProducts());
+            for(ProdRecEntity prodRecOfRecipe:prodRecListOfRecipe){//у каждого рецепта идем по списку prodRec сущностей
+                if(!prodRecListOfProducts.contains(prodRecOfRecipe)){//если такой сущности нет в переданных продуктах и
+                    if(prodRecOfRecipe.getProductImportance()>5){//и ее важность больше пяти, то мы не можем выдать этот рецепт
+                        recipeEntitiesForDelition.add(prodRecOfRecipe.getRecipe());
+                        break;
+                    }
+                }
+            }
+        }
+        if(!recipeEntitiesForDelition.isEmpty()) {
+            recipeEntities.removeAll(recipeEntitiesForDelition);
+        }
+        for(RecipeEntity recipe:recipeEntities) {
+            RecipeModelPure model = new RecipeModelPure();
+            model.toModel(recipe);
+            if(!finalRecipes.contains(model)) {
                 finalRecipes.add(model);
             }
         }
-
         return finalRecipes;
-        //заполняем мапу конкретным реуептом и числом его повторений. Если чисто повторений реуепта равно числу продуктов то это наш рецепт.
-        //Для оптимизации можно сделать так: если новый рецепт не равен рецептам первого продукта, то в мапу его не добавляем
-        //первым в таком случае лучше взять продукт с наименьшим к-м рецептов!
+//        productsList.sort(new Comparator<UserProductsEntity>() {
+//            @Override
+//            public int compare(UserProductsEntity o1, UserProductsEntity o2) {
+//                Integer size1 = o1.getProduct().getRecipes().size();
+//                Integer size2 = o2.getProduct().getRecipes().size();
+//                return size1.compareTo(size2);
+//            }
+//        });
+//        UserProductsEntity usersProdFirst = productsList.get(0);
+//        for(ProdRecEntity prodRec:usersProdFirst.getProduct().getRecipes()){
+//
+//            recipeEntities.add(prodRec.getRecipe());
+//        }
+//        productsList.remove(usersProdFirst);
+//
+//        for(UserProductsEntity usersProd:productsList){
+//            for(RecipeEntity recipe:recipeEntities){
+//            EmbProdRecId embId = new EmbProdRecId();
+//                embId.setRecIdComp(recipe.getRecipeId());
+//                embId.setProdIdComp(usersProd.getProduct().getIdProd());
+//                Optional<ProdRecEntity> prodRec = prodRecRepo.findById(embId);
+//                if(prodRec.isPresent()){
+//                    if(!usersProd.getProduct().getRecipes().contains(prodRec.get())){
+//                        recipeEntities.remove(recipe);
+//                    };
+//                }else {recipeEntities.remove
+//            }
+//        }
+//        for(UserProductsEntity usersProd:productsList){
+//            System.out.println(usersProd.getProduct().getRecipes().size());
+//            for(ProdRecEntity prodRec:usersProd.getProduct().getRecipes()){
+//                RecipeEntity recipe =prodRec.getRecipe();
+//                if(recipeMap.containsKey(recipe)){
+//                    recipeMap.replace(recipe,recipeMap.get(recipe)+1);
+//                }
+//            }
+//        }
+//        for(RecipeEntity key:recipeMap.keySet()){
+//            if(recipeMap.get(key).equals(productsCount)){
+//                RecipeModelPure model = new RecipeModelPure();
+//                model.toModel(key);
+//                finalRecipes.add(model);
+//            }
+//        }
     }
 
 
